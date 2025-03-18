9d490440c27038df6b86f106377a9ceac27081ad
juanaw6 <alfredwidjaya64@gmail.com>
2025-03-19 01:30:22 +0700

    feat: add rmsnorm and l1 l2 reg

--- src/model.py
+++ src/model.py
@@ -4,40 +4,69 @@ import matplotlib.pyplot as plt
 from activations import ELUActivation, LeakyReLUActivation, LinearActivation, ReLUActivation, SigmoidActivation, TanhActivation, SoftmaxActivation
 from losses import CategoricalCrossEntropy, MSELoss, BinaryCrossEntropy
 from initializers import initialize_weights
+from norm import RMSNorm
 from utils import visualize_network
 
 
 class Layer:
-    def __init__(self, input_dim, output_dim, activation, weight_init_method="random_uniform", init_params=None):
+    def __init__(self, input_dim, output_dim, activation, weight_init_method="random_uniform", 
+                 init_params=None, use_rms_norm=False, l1_lambda=0.0, l2_lambda=0.0):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.activation = activation
+        self.use_rms_norm = use_rms_norm
+        self.l1_lambda = l1_lambda
+        self.l2_lambda = l2_lambda
+        
         self.W, self.b = initialize_weights(
             input_dim, output_dim, method=weight_init_method, init_params=init_params)
         self.dW = np.zeros_like(self.W)
         self.db = np.zeros_like(self.b)
+        
+        if use_rms_norm:
+            self.rms_norm = RMSNorm(output_dim)
 
     def forward(self, X):
         self.X = X
         self.Z = np.dot(X, self.W) + self.b
         self.A = self.activation.forward(self.Z)
+        
+        if self.use_rms_norm:
+            self.A = self.rms_norm.forward(self.A)
+            
         return self.A
 
     def backward(self, dA):
+        if self.use_rms_norm:
+            dA = self.rms_norm.backward(dA)
+            
         dZ = dA * self.activation.backward(self.Z)
         self.dW = np.dot(self.X.T, dZ) / self.X.shape[0]
         self.db = np.sum(dZ, axis=0, keepdims=True) / self.X.shape[0]
         dX = np.dot(dZ, self.W.T)
+        
+        if self.l1_lambda > 0:
+            self.dW += self.l1_lambda * np.sign(self.W)
+        if self.l2_lambda > 0:
+            self.dW += self.l2_lambda * self.W
+            
         return dX
 
     def update(self, learning_rate):
         self.W -= learning_rate * self.dW
         self.b -= learning_rate * self.db
+        
+        if self.use_rms_norm:
+            self.rms_norm.update(learning_rate)
 
 
 class FFNN:
-    def __init__(self, layers_config, loss, weight_init_method="random_uniform", init_params=None):
+    def __init__(self, layers_config, loss, weight_init_method="random_uniform",
+                 init_params=None, l1_lambda=0.0, l2_lambda=0.0):
         self.layers: list[Layer] = []
+        self.l1_lambda = l1_lambda
+        self.l2_lambda = l2_lambda
+
         if isinstance(loss, str):
             loss = loss.lower()
             if loss == "mse":
@@ -55,30 +84,38 @@ class FFNN:
             input_dim = layers_config[i-1][0]
             output_dim = layers_config[i][0]
             act_name = layers_config[i][1]
-            if act_name.lower() == "linear":
+            act_name = act_name.lower()
+            if act_name == "linear":
                 activation = LinearActivation()
-            elif act_name.lower() == "relu":
+            elif act_name == "relu":
                 activation = ReLUActivation()
-            elif act_name.lower() == "sigmoid":
+            elif act_name == "sigmoid":
                 activation = SigmoidActivation()
-            elif act_name.lower() == "tanh":
+            elif act_name == "tanh":
                 activation = TanhActivation()
-            elif act_name.lower() == "softmax":
+            elif act_name == "softmax":
                 activation = SoftmaxActivation()
-            elif act_name.lower() == "leakyrelu":
+            elif act_name == "leakyrelu":
                 alpha = 0.01
                 if len(layers_config[i]) > 2:
                     alpha = layers_config[i][2]
                 activation = LeakyReLUActivation(alpha=alpha)
-            elif act_name.lower() == "elu":
+            elif act_name == "elu":
                 alpha = 1.0
                 if len(layers_config[i]) > 2:
                     alpha = layers_config[i][2]
                 activation = ELUActivation(alpha=alpha)
             else:
                 raise ValueError("Unknown activation function: " + act_name)
-            layer = Layer(input_dim, output_dim, activation,
-                          weight_init_method, init_params)
+            
+            use_rms_norm = False
+            if len(layers_config[i]) > 2 and isinstance(layers_config[i][2], dict):
+                config = layers_config[i][2]
+                if 'rms_norm' in config:
+                    use_rms_norm = config['rms_norm']
+            
+            layer = Layer(input_dim, output_dim, activation, 
+                          weight_init_method, init_params, use_rms_norm=use_rms_norm)
             self.layers.append(layer)
 
     def forward(self, X):
@@ -104,10 +141,31 @@ class FFNN:
             gradient = self.loss_func.gradient(y_true, y_pred)
             for layer in reversed(self.layers):
                 gradient = layer.backward(gradient)
+                
+                if self.l1_lambda > 0:
+                    l1_grad = np.sign(layer.W)
+                    layer.dW += self.l1_lambda * l1_grad
+                
+                if self.l2_lambda > 0:
+                    l2_grad = layer.W
+                    layer.dW += self.l2_lambda * l2_grad
+
 
     def update(self, learning_rate):
         for layer in self.layers:
             layer.update(learning_rate)
+            
+    def _calculate_reg_loss(self):
+        l1_reg = 0
+        l2_reg = 0
+        
+        for layer in self.layers:
+            if self.l1_lambda > 0:
+                l1_reg += np.sum(np.abs(layer.W))
+            if self.l2_lambda > 0:
+                l2_reg += np.sum(np.square(layer.W))
+        
+        return self.l1_lambda * l1_reg + 0.5 * self.l2_lambda * l2_reg
 
     def train(self, X_train, y_train, X_val=None, y_val=None, epochs=10, batch_size=32, learning_rate=0.01, verbose=1):
         
@@ -140,7 +198,8 @@ class FFNN:
                 
                 y_pred = self.forward(X_batch)
                 batch_loss = self.loss_func.loss(y_batch, y_pred)
-                epoch_loss += batch_loss
+                reg_loss = self._calculate_reg_loss() 
+                epoch_loss += batch_loss + reg_loss
                 
                 self.backward(y_batch, y_pred)
                 self.update(learning_rate)
@@ -176,7 +235,7 @@ class FFNN:
         return history
 
     def summary(self):
-        print("FFNN Model's Layer:")
+        print("FFNN Model Layer:")
         for idx, layer in enumerate(self.layers):
             print(
                 f"Layer {idx}: Input Dim = {layer.input_dim}, Output Dim = {layer.output_dim}, Activation = {type(layer.activation).__name__}")
